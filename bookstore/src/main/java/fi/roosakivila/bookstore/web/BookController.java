d3f59e9428bf5058b03bc25d07a190ac83327778
Roosa Kivilä <roosakivila@Roosa-MacBook-Air.local>
2025-09-14 19:51:27 +0300

    Tehtävä C3.2 valmis

--- bookstore/src/main/java/fi/roosakivila/bookstore/web/BookController.java
+++ bookstore/src/main/java/fi/roosakivila/bookstore/web/BookController.java
@@ -4,13 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import fi.roosakivila.bookstore.domain.BookRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     // GET request /index
     @GetMapping("/index")
     public String getBooks(Model model) {
         return "index"; // index.html
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist"; // booklist.html
     }
 
 }
