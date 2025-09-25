92f193f45c2e3fa7887ff70235f4153c2f005f6f
Roosa Kivilä <roosakivila@Roosa-MacBook-Air.local>
2025-09-25 15:23:30 +0300

    Tehtävä C5.2. valmis

--- bookstore/src/main/java/fi/roosakivila/bookstore/domain/BookRepository.java
+++ bookstore/src/main/java/fi/roosakivila/bookstore/domain/BookRepository.java
@@ -1,7 +1,9 @@
 package fi.roosakivila.bookstore.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
 
 }
